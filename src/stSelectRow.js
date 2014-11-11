46a118fad48b59564d075a791a23b9e9399cfabf
Rimvydas Urbonas <rimvydas.urbonas@devbridge.com>
2014-11-11 08:40:34 +0200

    Created smart table config for selected row class.

--- src/stSelectRow.js
+++ src/stSelectRow.js
@@ -1,26 +1,36 @@
 ng.module('smart-table')
-    .directive('stSelectRow', function () {
+    .directive('stSelectRow', ['smartTableConfig', function (smartTableConfig) {
         return {
             restrict: 'A',
             require: '^stTable',
             scope: {
-                row: '=stSelectRow'
+                row: '=stSelectRow',
+                onSelected: '&'
             },
             link: function (scope, element, attr, ctrl) {
                 var mode = attr.stSelectMode || 'single';
+                var onSelected = null;
+
+                attr.$observe('onSelected', function (value) {
+                    onSelected = scope.$parent.$eval(value);
+                });
+
                 element.bind('click', function () {
                     scope.$apply(function () {
                         ctrl.select(scope.row, mode);
+                        if (onSelected) {
+                            onSelected(scope.row);
+                        }
                     });
                 });
 
                 scope.$watch('row.isSelected', function (newValue, oldValue) {
                     if (newValue === true) {
-                        element.addClass('st-selected');
+                        element.addClass(smartTableConfig.selectedRowClass);
                     } else {
-                        element.removeClass('st-selected');
+                        element.removeClass(smartTableConfig.selectedRowClass);
                     }
                 });
             }
         };
-    });
+    }]);
--- src/stTable.js
+++ src/stTable.js
@@ -1,4 +1,56 @@
 ng.module('smart-table')
+    .provider('smartTableConfig', function () {
+                        var config = {
+                            selectedRowClass: 'st-selected'
+                        };
+                        var emptyController = function () { };
+
+                        function stringSetter(setting, value) {
+                            if (typeof value === 'string') {
+                                config[setting] = value;
+                            }
+                        }
+
+                        function controllerSetter(setting, value) {
+                            if (value && (ng.isString(value) && value.length || ng.isFunction(value) || ng.isArray(value))) {
+                                config[setting] = value;
+                            } else {
+                                config[setting] = emptyController;
+                            }
+                        }
+
+                        function numberSetter(setting, value) {
+                            if (typeof value === 'number') {
+                                config[setting] = value;
+                            }
+                        }
+
+                        var setters = {
+                            'selectedRowClass': stringSetter
+                        };
+
+                        this.$get = function () {
+                            return config;
+                        };
+
+                        this.set = function (name, value) {
+                            var fn, key, props, i;
+                            if (typeof name === 'string') {
+                                fn = setters[name];
+                                if (fn) {
+                                    fn(name, value);
+                                }
+                            } else if (typeof name === 'object') {
+                                props = Object.keys(name);
+                                for (i = 0; i < props.length; ++i) {
+                                    key = props[i];
+                                    fn = setters[key];
+                                    if (fn) {
+                                        fn(key, name[key]);
+                                    }
+                                }
+                            }
+                        };})
     .controller('stTableController', ['$scope', '$parse', '$filter', '$attrs', function StTableController($scope, $parse, $filter, $attrs) {
         var propertyName = $attrs.stTable;
         var displayGetter = $parse(propertyName);
